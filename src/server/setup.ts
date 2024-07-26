4f40984380580cfbe4fdd6100f7705bc9a958318
nodeexcel <atul.etech2011@gmail.com>
2024-07-26 15:17:47 +0530

    added products

--- src/server/setup.ts
+++ src/server/setup.ts
@@ -79,3 +79,94 @@ const addAllPages = async db => {
     is_system: true,
   })
 }
+
+const addAllProducts = async db => {
+  const productCategoriesCount = await db
+    .collection("productCategories")
+    .countDocuments({})
+
+  const productsCount = await db.collection("products").countDocuments({})
+
+  const productsNotExists = productCategoriesCount === 0 && productsCount === 0
+
+  if (productsNotExists) {
+    const catA = await db.collection("productCategories").insertOne({
+      name: "Category A",
+      slug: "category-a",
+      image: "",
+      parent_id: null,
+      enabled: true,
+    })
+
+    const catB = await db.collection("productCategories").insertOne({
+      name: "Category B",
+      slug: "category-b",
+      image: "",
+      parent_id: null,
+      enabled: true,
+    })
+
+    const catC = await db.collection("productCategories").insertOne({
+      name: "Category C",
+      slug: "category-c",
+      image: "",
+      parent_id: null,
+      enabled: true,
+    })
+
+    const catA1 = await db.collection("productCategories").insertOne({
+      name: "Subcategory 1",
+      slug: "category-a-1",
+      image: "",
+      parent_id: catA.insertedId,
+      enabled: true,
+    })
+
+    const catA2 = await db.collection("productCategories").insertOne({
+      name: "Subcategory 2",
+      slug: "category-a-2",
+      image: "",
+      parent_id: catA.insertedId,
+      enabled: true,
+    })
+
+    const catA3 = await db.collection("productCategories").insertOne({
+      name: "Subcategory 3",
+      slug: "category-a-3",
+      image: "",
+      parent_id: catA.insertedId,
+      enabled: true,
+    })
+
+    await db.collection("products").insertOne({
+      name: "Product A",
+      slug: "product-a",
+      category_id: catA.insertedId,
+      regular_price: 950,
+      stock_quantity: 999,
+      enabled: true,
+      discontinued: false,
+      attributes: [
+        { name: "Brand", value: "Brand A" },
+        { name: "Size", value: "M" },
+      ],
+    })
+
+    await db.collection("products").insertOne({
+      name: "Product B",
+      slug: "product-b",
+      category_id: catA.insertedId,
+      regular_price: 1250,
+      stock_quantity: 999,
+      enabled: true,
+      discontinued: false,
+      attributes: [
+        { name: "Brand", value: "Brand B" },
+        { name: "Size", value: "L" },
+      ],
+    })
+
+    winston.info("- Added products")
+  }
+}
+
